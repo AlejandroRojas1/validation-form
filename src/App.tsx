c5b38bc7acce0cc5b206187c2c7b868d4db37f16
Alejandro Rojas <alejandrofabianrojas64@gmail.com>
2024-11-09 05:24:08 -0300

    finish the validation form

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,71 @@
-import { useForm } from "react-hook-form"
+import { SubmitHandler, useForm } from "react-hook-form"
+
+type FormData = {
+  name: string,
+  email: string,
+  password: string
+}
 
 function App() {
-  const {register, handleSubmit} = useForm()
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>()
+  const onSubmit: SubmitHandler<FormData> = data => {
+    console.log(errors.name)
+    console.log(data)
+  }
 
   return (
     <>
-    <div className="w-full mt-32 flex flex-col justify-center">
-      <h1 className="text-center font-bold text-xl">Formulario para validar</h1>
-      <form className="p-4 flex flex-col gap-y-2 mx-auto my-10 bg-blue-100 rounded" onSubmit={handleSubmit(data=>{console.log(data)})}>
+      <div className="w-full mt-32 flex flex-col justify-center">
+        <h1 className="text-center font-bold text-xl">Formulario para validar</h1>
 
-        <label htmlFor="name">nombre: </label>
-        <input className="rounded p-2" placeholder="nombre" id="name" type="text" 
-        {...register("name")}
-        />
-        
-        <label htmlFor="email">email: </label>
-        <input className="rounded p-2" placeholder="email" id="email" type="email" 
-        {...register("email")}/>
+        <form className="p-4 flex flex-col gap-y-2 mx-auto my-10 bg-blue-100 rounded min-w-80 max-w-80" onSubmit={handleSubmit(onSubmit)}>
 
-        <label htmlFor="password">contraseña: </label>
-        <input className="rounded p-2" placeholder="contraseña" id="password" type="password" 
-        {...register("password")}/>
+          <label htmlFor="name">nombre: </label>
+          <div className={`${errors.name && 'bg-red-600 rounded'} flex flex-col`}>
+            <input
+              className={`rounded p-2 focus:outline-none focus:border-2 ${!errors.name && 'focus:border-blue-500'} ${errors.name && 'border-2 border-red-600 focus:outline-none focus:border-2 focus:border-red-600'}`}
+              placeholder="nombre"
+              id="name"
+              type="text"
 
-        <button className="rounded p-2 bg-red-200 mt-4 hover:cursor-pointer font-bold text-gray-500" type="submit" >Enviar</button>
-      </form>
-    </div>
+              {...register("name", { required: true, maxLength: 20, pattern: /^[A-Za-z]+$/i })}
+            />
+            {errors.name && <p className="bg-red-600 text-white rounded px-2 max-w-full">Ingrese un nombre, no debe contener numeros ni caracteres especiales</p>}
+          </div>
+
+          <label htmlFor="email">email: </label>
+          <div className={`${errors.email && 'bg-red-600 rounded'} flex flex-col`}>
+            <input
+              className={`rounded p-2 focus:outline-none focus:border-2 ${!errors.email && 'focus:border-blue-500'} 
+              
+              ${errors.email && 'border-2 border-red-600 focus:outline-none focus:border-2 focus:border-red-600'}`}
+
+              placeholder="email"
+              id="email"
+              type="email"
+              {...register("email", {required: true})}
+            />
+            {errors.email && <p className="bg-red-600 text-white rounded px-2 max-w-full">Ingrese un email valido</p>}
+          </div>
+
+          <label htmlFor="password">contraseña: </label>
+          <div className={`${errors.password && 'bg-red-600 rounded'} flex flex-col`}>
+            <input 
+            className={`rounded p-2 focus:outline-none focus:border-2 ${!errors.password && 'focus:border-blue-500'}  
+            
+            ${errors.password && 'border-2 border-red-600 focus:outline-none focus:border-2 focus:border-red-600'}`}
+
+            placeholder="contraseña" 
+            id="password" 
+            type="password"
+              {...register("password", {required: true, minLength: 8, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]+$/})} 
+              />
+              {errors.password && <p className="bg-red-600 text-white rounded px-2 max-w-full">La contraseña debe contener almenos una mayuscula, una minuscula, un numero y debe ser de almenos 8 caracteres</p>}
+          </div>
+
+          <button className="rounded p-2 bg-red-200 mt-4 hover:cursor-pointer font-bold text-gray-500" type="submit" >Enviar</button>
+        </form>
+      </div>
     </>
   )
 }
